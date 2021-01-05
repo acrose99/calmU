daed8c3147841a1ed4338d802be66883dbac50f4
acrose99 <acrosemail@gmail.com>
2021-01-05 04:23:29 -0600

    FEAT: Assets finally drawn!!!!

---
+++ src/container/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+    let src = '../images/Homepage/Logo.svg'
+</script>
+
+<div class="container">
+    <img  alt="Logo" src={src}>
+    <h1 style="font-size: 36px">Calming Ambience, for as long as you want</h1>
+</div>
+
+<style>
+    .container {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        color: white;
+        justify-content: center;
+        text-align: center;
+       height: 100%;
+        background-color: #279DA4;
+    }
+</style>
