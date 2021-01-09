1961adf292aba8bf92afb5a2ab212ac6ac34cf86
acrose99 <acrosemail@gmail.com>
2021-01-08 19:57:25 -0600

    github actions

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
-import express from 'express'
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
 
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
 
-
-express()
-    .listen(3000, err => {
+polka() // You can also use Express
+    .use(
+        'calmU',
+        compression({ threshold: 0 }),
+        sirv('static', { dev }),
+        sapper.middleware()
+    )
+    .listen(PORT, err => {
         if (err) console.log('error', err);
     });
